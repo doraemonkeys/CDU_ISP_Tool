f37caa304351d6cb647b77ca2a477bb033be7682
doraemon <doraemonkey@foxmail.com>
2022-08-31 12:07:57 +0800

    fix bug

--- utils/utils.go
+++ utils/utils.go
@@ -148,16 +148,15 @@ func PressToContinue(ch chan bool) {
 	close(ch)
 }
 
-//不等待执行完毕就返回
-//如果params中有转义字符需要自己处理
-//dir为cmd命令执行的位置,传入空值则为默认路径
-func Cmd_NoWait(dir string, params []string) (string, error) {
-	cmd := exec.Command("cmd")
+//不等待执行完毕就返回,如果params中有转义字符需要自己处理,
+//dir为cmd命令执行的位置,传入空值则为默认路径。
+func Cmd_NoWait(dir string, params []string) (cmd *exec.Cmd, err error) {
+	cmd = exec.Command("cmd")
 	cmd_in := bytes.NewBuffer(nil)
 	cmd.Stdin = cmd_in
-	cmd_out := bytes.NewBuffer(nil)
-	cmd.Stdout = cmd_out
-	cmd.Dir = dir
+	if dir != "" {
+		cmd.Dir = dir
+	}
 	command := ""
 	for i := 0; i < len(params); i++ {
 		command = command + params[i]
@@ -166,12 +165,15 @@ func Cmd_NoWait(dir string, params []string) (string, error) {
 		}
 	}
 	cmd_in.WriteString(command + "\n")
-	err := cmd.Start() //不等待执行完毕就返回
+	err = cmd.Start() //不等待执行完毕就返回
 	if err != nil {
-		return "", err
+		return cmd, err
 	}
-	output := cmd_out.Bytes()
-	return string(GbkToUtf8(output)), nil
+	//等待cmd已经读取指令
+	for cmd_in.Len() != 0 {
+		time.Sleep(time.Microsecond * 10)
+	}
+	return cmd, nil
 }
 
 func GbkToUtf8(b []byte) []byte {
