09bd92b612bf071fe4598d01e22f6085d345f41c
doraemon <doraemonkey@foxmail.com>
2022-09-11 13:36:40 +0800

    updated Version No.

--- model/model.go
+++ model/model.go
@@ -32,7 +32,7 @@ var Auto_Clock_IN_Success bool = false
 var UserConfigChanged bool = false
 
 //版本号
-var Version string = "v1.5.3"
+var Version string = "v1.5.4"
 
 type Update struct {
 	LanzouUrl                 string //蓝奏云地址
